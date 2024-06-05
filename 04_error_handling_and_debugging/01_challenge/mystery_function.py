6229818cda27ae18562df83d29d178eec55a8cb3
Takashi Nagakoshi <takashi.nagakoshi@woven-planet.global>
2024-06-05 22:35:35 +0900

    add lst[]

--- 04_error_handling_and_debugging/01_challenge/mystery_function.py
+++ 04_error_handling_and_debugging/01_challenge/mystery_function.py
@@ -1,6 +1,18 @@
 def mystery_function(lst):
     result = lst
     for i in range(len(lst)):
-        if i % 2 == 0:
-            result[i] = i ** 2
+        # インデックスではなくリストの要素の偶奇をチェックする
+        if lst[i] % 2 == 0:
+            result[i] = lst[i] ** 2
     return result
+    
+lst= [1, 2, 3, 4, 5]
+lst= [4, 1, 6, 2, 10]
+
+def main():
+    results = mystery_function(lst)
+    for result in results:
+        print(result)
+
+if __name__ == "__main__":
+    main()
