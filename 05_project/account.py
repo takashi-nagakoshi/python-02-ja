0f62f9d3baa7e1e98a554d7b240babaaf90c508c
rhashimoto <ryuta.hashimoto@woven-planet.global>
2024-06-12 18:38:24 +0900

    interest使用部分の削除．（本課題に直接関係しないため）

--- 05_project/account.py
+++ 05_project/account.py
@@ -28,10 +28,6 @@ class Account(ABC):
         else:
             return "Withdrawal denied. Insufficient funds or rules not met."
 
-    #利息を適用するためのメソッドで、サブクラスで上書き　？この場所にいるのか？
-    def apply_interest(self):
-        pass
-
     #取引履歴
     def get_transaction_history(self):
         return self.transaction_history
@@ -43,20 +39,12 @@ class Account(ABC):
 #_____________________________________________________________________________________________________#
 # Accountクラスを継承   貯蓄口座
 class SavingsAccount(Account):
-    def __init__(self, account_number, balance=0, interest_rate=0.01):
+    def __init__(self, account_number, balance=0):
         super().__init__(account_number, balance)
-        self.interest_rate = interest_rate
     
     # 残高が引き出し金額以上であれば可能
     def can_withdraw(self, amount):
         return self.balance >= amount
-    
-    # 金利を計算し、残高に追加します。また、取引履歴にも追加
-    def apply_interest(self):
-        interest = self.balance * self.interest_rate
-        self.balance += interest
-        self.transaction_history.append(f"Interest applied: {interest}")
-        return f"Interest applied: {interest}. New balance: {self.balance}"
     
     # 貯蓄口座情報を文字列で返す
     def __str__(self):
@@ -65,12 +53,12 @@ class SavingsAccount(Account):
 #_____________________________________________________________________________________________________#
 # 当座預金口座
 class CheckingAccount(Account):
-    def __init__(self, account_number, balance=0, interest_rate=0.02):
+    def __init__(self, account_number, balance=0):
         super().__init__(account_number, balance)
-        self.interest_rate = interest_rate
 
     def can_withdraw(self, amount):
-        return self.balance + self.interest_rate >= amount
+        # interest(利息)を考慮すればSavingAccountと処理が変わる可能性があるが，現状簡略のためSavingAccountと同じ処理になっている．
+        return self.balance >= amount
     
     def __str__(self):
         return f"Checking Account: {self.account_number}, Balance: {self.balance}, Interest Rate: {self.interest_rate}"
