b8bbe198ff96a8a8edd304330637eece714e574d
rhashimoto <ryuta.hashimoto@woven-planet.global>
2024-06-14 13:57:59 +0900

    ユーザーログインシステム，およびパスワード認証を追加

--- 05_project/bankingsystem.py
+++ 05_project/bankingsystem.py
@@ -1,15 +1,17 @@
 import bank as ClassBank
 
-KEY_CREATE_USER         = "1"
-KEY_CREATE_ACCOUNT      = "2"
-KEY_DEPOSIT             = "3"
-KEY_WITHDRAW            = "4"
-KEY_TRANSFER            = "5"
-KEY_VIEW_TRANS_HISTORY  = "6"
-KEY_VIEW_BALANCE        = "7"
-KEY_DELETE_ACCOUNT      = "8"
-KEY_GET_ACCOUNT_INFO    = "9"
-KEY_QUIT                = "10"
+KEY_LOGIN_USER          = "1"
+KEY_CREATE_USER         = "2"
+
+KEY_CREATE_ACCOUNT      = "0"
+KEY_DEPOSIT             = "1"
+KEY_WITHDRAW            = "2"
+KEY_TRANSFER            = "3"
+KEY_VIEW_TRANS_HISTORY  = "4"
+KEY_VIEW_BALANCE        = "5"
+KEY_GET_ACCOUNT_INFO    = "6"
+KEY_DELETE_ACCOUNT      = "7"
+KEY_QUIT                = "X"
 WAIT_ROW = "....."
 
 #_____________________________________________________________________________________________________#
@@ -19,39 +21,104 @@ class BankingSystem:
         self.bank = ClassBank.Bank()
 
     def run(self):
+        # Main menu 
         while True:
+            print("")
             print("############### Banking System Menu ###############")
+            print("### Main menu")
+            print(KEY_LOGIN_USER         + ". Login User")
             print(KEY_CREATE_USER        + ". Create User")
-            print(KEY_CREATE_ACCOUNT     + ". Create Account")
-            print(KEY_DEPOSIT            + ". Deposit")
-            print(KEY_WITHDRAW           + ". Withdraw")
-            print(KEY_TRANSFER           + ". Transfer")
-            print(KEY_VIEW_TRANS_HISTORY + ". View Transaction History")
-            print(KEY_VIEW_BALANCE       + ". View Balance")
-            print(KEY_DELETE_ACCOUNT     + ". Delete Account")
-            print(KEY_GET_ACCOUNT_INFO   + ". Get Account Info")
             print(KEY_QUIT               + ". Quit")
             print(WAIT_ROW)
             choice = input("Enter choice: ")
 
             if choice == KEY_CREATE_USER:
                 self.create_user()
-            elif choice == KEY_CREATE_ACCOUNT:
+            elif choice == KEY_LOGIN_USER:
+                user_id = self.login_user()
+                if user_id:
+                    self.run_user_menu(user_id)
+                else:
+                    continue
+            elif choice == KEY_QUIT:
+                break
+            else:
+                print("Invalid choice!")
+        return
+    
+    def login_user(self):
+        count = 0 # 同じ入力を何回システムから聞き返したか
+        count_max = 3 # 最大何回まで同じ入力をシステムから聞き返すか
+
+        # ユーザーIDの確認
+        while True:
+            user_id = input("Enter user ID: ")
+            is_use_id_exist = self.bank.check_user_id_exist(user_id)
+            if is_use_id_exist:
+                break
+            else:
+                count += 1
+                if count < count_max:
+                    print("... That user ID doesn't exist.")
+                    continue
+                else:
+                    print("... User login failed")
+                    return None
+        
+        # パスワードの確認
+        count = 0
+        while True:
+            user_passwd = input("Enter user password: ")
+            is_passwd_correct = self.bank.check_user_id_and_password_are_correct(user_id, user_passwd)
+            if is_passwd_correct:
+                print("... Login success")
+                return user_id
+            else:
+                count += 1
+                if count < count_max:
+                    print("... Incorrect password.")
+                    continue
+                else:
+                    print("... User login failed")
+                    return None
+
+
+    def run_user_menu(self, user_id):
+        while True:
+            print("")
+            print("############### Banking System Menu ###############")
+            print("### User menu (user_id = " + user_id + ")")
+            print(KEY_CREATE_ACCOUNT     + ". Create Account")
+            print(" --  --  --  --  --  -- ")
+            print(KEY_DEPOSIT            + ". Deposit")
+            print(KEY_WITHDRAW           + ". Withdraw")
+            print(KEY_TRANSFER           + ". Transfer")
+            print(" --  --  --  --  --  -- ")
+            print(KEY_VIEW_TRANS_HISTORY + ". View Transaction History")
+            print(KEY_VIEW_BALANCE       + ". View Balance")
+            print(KEY_GET_ACCOUNT_INFO   + ". Get Account Info")
+            print(" --  --  --  --  --  -- ")
+            print(KEY_DELETE_ACCOUNT     + ". Delete Account")
+            print(KEY_QUIT               + ". Quit")
+            print(WAIT_ROW)
+            choice = input("Enter choice: ")
+
+            if choice == KEY_CREATE_ACCOUNT:
                 self.create_account()
             elif choice == KEY_DEPOSIT:
-                self.deposit()
+                self.deposit(user_id)
             elif choice == KEY_WITHDRAW:
-                self.withdraw()
+                self.withdraw(user_id)
             elif choice == KEY_TRANSFER:
-                self.transfer()
+                self.transfer(user_id)
             elif choice == KEY_VIEW_TRANS_HISTORY:
-                self.view_transaction_history()
+                self.view_transaction_history(user_id)
             elif choice == KEY_VIEW_BALANCE:
-                self.view_balance()
-            elif choice == KEY_DELETE_ACCOUNT:
-                self.delete_account()
+                self.view_balance(user_id)
             elif choice == KEY_GET_ACCOUNT_INFO:
-                self.get_account_info()
+                self.get_account_info(user_id)
+            elif choice == KEY_DELETE_ACCOUNT:
+                self.delete_account(user_id)
             elif choice == KEY_QUIT:
                 break
             else:
@@ -64,10 +131,11 @@ class BankingSystem:
             # ユーザー入力
             user_id = input("Enter user ID: ")
             name = input("Enter user name: ")
+            passwd = input("Enter user password: ")
             print(WAIT_ROW)
 
             # ユーザ登録処理
-            if self.bank.create_user(user_id, name):
+            if self.bank.create_user(user_id, name, passwd):
                 print("User created successfully!")
             else:
                 print("User already exists!")
@@ -97,10 +165,9 @@ class BankingSystem:
 
     #\__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ 
     # 口座への入金
-    def deposit(self):
+    def deposit(self, user_id):
         # ユーザ入力
         try:
-            user_id = input("Enter user ID: ")
             account_number = input("Enter account number: ")
             amount = float(input("Enter amount to deposit: "))
             print(WAIT_ROW)
@@ -115,10 +182,9 @@ class BankingSystem:
 
     #\__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ 
     # 口座からの出金
-    def withdraw(self):
+    def withdraw(self, user_id):
         try:
             # ユーザ入力
-            user_id = input("Enter user ID: ")
             account_number = input("Enter account number: ")
             amount = float(input("Enter amount to withdraw: "))
             print(WAIT_ROW)
@@ -133,10 +199,9 @@ class BankingSystem:
 
     #\__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ 
     # 口座から別口座への送金
-    def transfer(self):
+    def transfer(self, user_id_from):
         try:
             # ユーザ入力：送金元の情報
-            user_id_from = input("Enter sender user ID: ")
             account_number_from = input("Enter sender account number: ")
 
             # ユーザ入力：送金先の情報
@@ -158,10 +223,9 @@ class BankingSystem:
 
     #\__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ 
     # 口座の取引履歴を表示
-    def view_transaction_history(self):
+    def view_transaction_history(self, user_id):
         try:
             # ユーザ入力
-            user_id = input("Enter user ID: ")
             account_number = input("Enter account number: ")
             print(WAIT_ROW)
             
@@ -173,10 +237,9 @@ class BankingSystem:
 
     #\__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ 
     # 口座の残高を表示
-    def view_balance(self):
+    def view_balance(self, user_id):
         try:
             # ユーザ入力
-            user_id = input("Enter user ID: ")
             account_number = input("Enter account number: ")
             print(WAIT_ROW)
 
@@ -187,10 +250,9 @@ class BankingSystem:
             print(f"Error viewing balance: {e}")
     #\__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ 
     # 口座を解約(消去)
-    def delete_account(self):
+    def delete_account(self, user_id):
         try:
             # ユーザ処理
-            user_id = input("Enter user ID: ")
             account_number = input("Enter account number: ")
             print(WAIT_ROW)
 
@@ -204,10 +266,9 @@ class BankingSystem:
             print(f"Error deleting account: {e}")
     #\__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ \__ 
     # 口座の情報を表示
-    def get_account_info(self):
+    def get_account_info(self, user_id):
         try:
             # ユーザ処理
-            user_id = input("Enter user ID: ")
             account_number = input("Enter account number: ")
             print(WAIT_ROW)
 
